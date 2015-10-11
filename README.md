c3fdec97e94514d0cfa070c5048a5d7f1d1c78df
Niklas Baudy <niklas.baudy@vanniktech.de>
2015-10-11 13:29:13 +0200

    Initial version of plugin. Plus v0.1.0

--- README.md
+++ README.md
@@ -1,3 +1,34 @@
 # gradle-android-junit-jacoco-plugin
 
 Gradle plugin that generates JaCoCo reports from an Android Gradle Project.
+
+# Set up
+
+## Root build.gradle
+
+```groovy
+apply plugin: "com.vanniktech.android.junit.jacoco"
+
+buildscript {
+    repositories {
+        maven {
+            url "https://plugins.gradle.org/m2/"
+        }
+    }
+    dependencies {
+        classpath "gradle.plugin.com.vanniktech:gradle-android-junit-jacoco-plugin:0.1.0"
+    }
+}
+```
+
+## Jacoco Reports
+
+```groovy
+./gradlew jacocoReport
+```
+
+# License
+
+Copyright (C) 2014-2015 Vanniktech - Niklas Baudy
+
+Licensed under the Apache License, Version 2.0
---
+++ src/main/groovy/com/vanniktech/android/junit/jacoco/Generation.groovy
@@ -0,0 +1,53 @@
+package com.vanniktech.android.junit.jacoco
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.testing.jacoco.tasks.JacocoReport
+
+class Generation implements Plugin<Project> {
+    @Override
+    void apply(final Project project) {
+        project.subprojects { subProject ->
+            subProject.plugins.apply('jacoco')
+
+            jacoco {
+                version '0.7.2.201409121644'
+            }
+
+            subProject.task("jacocoReport", type: JacocoReport, dependsOn: 'testDebugUnitTest') {
+                group = 'Reporting'
+                description = 'Generate Jacoco coverage reports after running tests.'
+
+                reports {
+                    xml {
+                        enabled = true
+                        destination "${subProject.buildDir}/reports/jacoco/jacoco.xml"
+                    }
+                    html {
+                        enabled = true
+                        destination "${subProject.buildDir}/reports/jacoco"
+                    }
+                }
+
+                classDirectories = fileTree(
+                        dir: 'build/intermediates/classes/debug',
+                        excludes: [
+                                '**/R*.class',
+                                '**/BuildConfig*'
+                        ]
+                )
+
+                sourceDirectories = files('src/main/java')
+                executionData = files('build/jacoco/testDebugUnitTest.exec')
+
+                doFirst {
+                    files('build/intermediates/classes/debug').getFiles().each { file ->
+                        if (file.name.contains('$$')) {
+                            file.renameTo(file.path.replace('$$', '$'))
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
