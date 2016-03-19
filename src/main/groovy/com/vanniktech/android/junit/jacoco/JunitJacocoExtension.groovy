b437313d93c3bb312f997286f889e024d55d10ed
Niklas Baudy <niklas.baudy@vanniktech.de>
2016-03-19 13:21:58 +0100

    Add extension to configure jacoco version

---
+++ src/main/groovy/com/vanniktech/android/junit/jacoco/JunitJacocoExtension.groovy
@@ -0,0 +1,13 @@
+package com.vanniktech.android.junit.jacoco
+
+/**
+ * Extension for junit jacoco
+ * @since 0.3.0
+ */
+class JunitJacocoExtension {
+    /**
+     * define the version of jacoco which should be used
+     * @since 0.3.0
+     */
+    String jacocoVersion = '0.7.2.201409121644'
+}
