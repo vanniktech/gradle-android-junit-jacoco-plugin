28ef2b09b45094e333c6afefc9ea3076af698f50
Niklas Baudy <niklas.baudy@vanniktech.de>
2016-03-20 13:11:58 +0100

    Implement features for version 0.3.0
    Clean imports
    
    Fix tests
    
    Fix some more things
    
    Fix minor bug
    
    Tet some more stuff
    
    Fix some more things
    
    Clean up
    
    Java depends on test and some more tests
    
    Change java executionData name
    
    Clean up

--- src/main/groovy/com/vanniktech/android/junit/jacoco/Generation.groovy
+++ src/main/groovy/com/vanniktech/android/junit/jacoco/Generation.groovy
@@ -6,89 +6,140 @@ import org.gradle.testing.jacoco.tasks.JacocoReport
 
 class Generation implements Plugin<Project> {
     @Override
-    void apply(final Project project) {
-        project.extensions.create('junitJacoco', JunitJacocoExtension)
+    void apply(final Project rootProject) {
+        rootProject.extensions.create('junitJacoco', JunitJacocoExtension)
 
-        project.subprojects { subProject ->
-            subProject.plugins.apply('jacoco')
+        rootProject.subprojects { subProject ->
+            afterEvaluate {
+                final def extension = rootProject.junitJacoco
 
-            subProject.jacoco {
-                toolVersion project.junitJacoco.jacocoVersion
+                addJacoco(subProject, extension)
+            }
+        }
+    }
+
+    protected static boolean addJacoco(final Project subProject, final JunitJacocoExtension extension) {
+        if (!shouldIgnore(subProject, extension)) {
+            if (isAndroidProject(subProject)) {
+                addJacocoAndroid(subProject, extension)
+                return true
+            } else if (isJavaProject(subProject)) {
+                addJacocoJava(subProject, extension)
+                return true
+            }
+        }
+
+        return false
+    }
+
+    private static void addJacocoJava(final Project subProject, final extension) {
+        subProject.plugins.apply('jacoco')
+
+        subProject.jacoco {
+            toolVersion extension.jacocoVersion
+        }
+
+        subProject.jacocoTestReport {
+            dependsOn 'test'
+
+            group = 'Reporting'
+            description = 'Generate Jacoco coverage reports.'
+
+            reports {
+                xml.enabled = true
+                html.enabled = true
             }
 
-            subProject.task('jacocoReport', type: JacocoReport, dependsOn: 'testDebugUnitTest') {
+            classDirectories = subProject.fileTree(
+                    dir: 'build/classes/main/',
+                    excludes: getExcludes()
+            )
+
+            final def coverageSourceDirs = [
+                    'src/main/java',
+            ]
+
+            additionalSourceDirs = subProject.files(coverageSourceDirs)
+            sourceDirectories = subProject.files(coverageSourceDirs)
+            executionData = subProject.files("${subProject.buildDir}/jacoco/test.exec")
+        }
+
+        subProject.check.dependsOn 'jacocoTestReport'
+    }
+
+    private static void addJacocoAndroid(final Project subProject, final extension) {
+        subProject.plugins.apply('jacoco')
+
+        subProject.jacoco {
+            toolVersion extension.jacocoVersion
+        }
+
+        final def buildTypes = subProject.android.buildTypes.collect { type -> type.name }
+
+        buildTypes.each { buildTypeName ->
+            final def taskName = "jacocoTestReport${buildTypeName.capitalize()}"
+            final def testTaskName = "test${buildTypeName.capitalize()}UnitTest"
+
+            subProject.task(taskName, type: JacocoReport, dependsOn: testTaskName) {
                 group = 'Reporting'
-                description = 'Generate Jacoco coverage reports after running tests.'
+                description = "Generate Jacoco coverage reports after running ${buildTypeName} tests."
 
                 reports {
                     xml {
                         enabled = true
-                        destination "${subProject.buildDir}/reports/jacoco/jacoco.xml"
+                        destination "${subProject.buildDir}/reports/jacoco/${buildTypeName}/jacoco.xml"
                     }
                     html {
                         enabled = true
-                        destination "${subProject.buildDir}/reports/jacoco"
+                        destination "${subProject.buildDir}/reports/jacoco/${buildTypeName}"
                     }
                 }
 
-                classDirectories = fileTree(
-                        dir: 'build/intermediates/classes/debug',
-                        excludes: ['**/R.class',
-                                   '**/R$*.class',
-                                   '**/*$$*',
-                                   '**/*$ViewInjector*.*',
-                                   '**/*$ViewBinder*.*',
-                                   '**/BuildConfig.*',
-                                   '**/Manifest*.*',
-                                   '**/*$Lambda$*.*', // Jacoco can not handle several "$" in class name.
-                                   '**/*Dagger*.*', // Dagger auto-generated code.
-                                   '**/*MembersInjector*.*', // Dagger auto-generated code.
-                                   '**/*_Provide*Factory*.*' // Dagger auto-generated code.
-                       ]
+                classDirectories = subProject.fileTree(
+                        dir: "build/intermediates/classes/${buildTypeName}",
+                        excludes: getExcludes()
                 )
 
-                sourceDirectories = files('src/main/java')
-                executionData = files('build/jacoco/testDebugUnitTest.exec')
+                final def coverageSourceDirs = [
+                        'src/main/java',
+                        "src/$buildTypeName/java"
+                ]
 
-                doFirst {
-                    files('build/intermediates/classes/debug').getFiles().each { file ->
-                        if (file.name.contains('$$')) {
-                            file.renameTo(file.path.replace('$$', '$'))
-                        }
-                    }
-                }
-            }
-        }
-
-        project.plugins.apply('jacoco')
-
-        project.jacoco {
-            toolVersion project.junitJacoco.jacocoVersion
-        }
-
-        project.task('jacocoFullReport', type: JacocoReport, group: 'Coverage reports') {
-            group = 'Reporting'
-            description = 'Generate Jacoco coverage reports aggregated from all subprojects.'
-            dependsOn(project.subprojects.jacocoReport)
-
-            executionData = project.files(project.subprojects.jacocoReport.executionData)
-            classDirectories = project.files(project.subprojects.jacocoReport.classDirectories)
-            sourceDirectories = project.files(project.subprojects.jacocoReport.sourceDirectories)
-
-            reports {
-                xml {
-                    enabled = true
-                    destination "${project.buildDir}/reports/jacoco/full/jacoco.xml"
-                }
-                html {
-                    enabled = true
-                    destination "${project.buildDir}/reports/jacoco/full"
-                }
+                additionalSourceDirs = subProject.files(coverageSourceDirs)
+                sourceDirectories = subProject.files(coverageSourceDirs)
+                executionData = subProject.files("${subProject.buildDir}/jacoco/${testTaskName}.exec")
             }
 
-            doFirst {
-                executionData = project.files(executionData.findAll { it.exists() })
-            }
+            subProject.check.dependsOn "${taskName}"
         }
+    }
+
+    private static ArrayList<String> getExcludes() {
+        ['**/R.class',
+         '**/R$*.class',
+         '**/*$$*',
+         '**/*$ViewInjector*.*',
+         '**/*$ViewBinder*.*',
+         '**/BuildConfig.*',
+         '**/Manifest*.*',
+         '**/*$Lambda$*.*', // Jacoco can not handle several "$" in class name.
+         '**/*Dagger*.*', // Dagger auto-generated code.
+         '**/*MembersInjector*.*', // Dagger auto-generated code.
+         '**/*_Provide*Factory*.*' // Dagger auto-generated code.
+        ]
+    }
+
+    protected static boolean isAndroidProject(final Project project) {
+        final boolean isAndroidLibrary = project.plugins.hasPlugin('com.android.library')
+        final boolean isAndroidApp = project.plugins.hasPlugin('com.android.application')
+        return isAndroidLibrary || isAndroidApp
+    }
+
+    protected static boolean isJavaProject(final Project project) {
+        return project.plugins.hasPlugin('org.gradle.java')
+    }
+
+    private static boolean shouldIgnore(final Project project, final JunitJacocoExtension extension) {
+        return extension.ignoreProjects?.contains(project.name)
     }
 }
--- src/test/groovy/com/vanniktech/android/junit/jacoco/JunitJacocoExtensionTest.groovy
+++ src/test/groovy/com/vanniktech/android/junit/jacoco/JunitJacocoExtensionTest.groovy
@@ -4,7 +4,10 @@ import org.junit.Test
 
 public class JunitJacocoExtensionTest {
     @Test
-    public void jacocoVersion() {
-        assert new JunitJacocoExtension().jacocoVersion == '0.7.2.201409121644'
+    public void defaults() {
+        def extension = new JunitJacocoExtension()
+
+        assert extension.jacocoVersion == '0.7.2.201409121644'
+        assert extension.ignoreProjects.size() == 0
     }
 }
