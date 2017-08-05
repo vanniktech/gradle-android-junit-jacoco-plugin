04d29155229d85dfda9ded7ee8da7610ebb909ee
Niklas Baudy <niklas.baudy@vanniktech.de>
2017-08-05 23:11:01 +0200

    One more try at trying to fix codecov. (#79)

--- .buildscript/deploy_snapshot.sh
+++ .buildscript/deploy_snapshot.sh
@@ -22,6 +22,6 @@ elif [ "$TRAVIS_BRANCH" != "$BRANCH" ]; then
   echo "Skipping snapshot deployment: wrong branch. Expected '$BRANCH' but was '$TRAVIS_BRANCH'."
 else
   echo "Deploying snapshot..."
-  ./gradlew clean uploadArchives
+  ./gradlew clean build uploadArchives
   echo "Snapshot deployed!"
 fi
