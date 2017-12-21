159e1176693a572474b7d9cf94ebcdccd5936b91
Niklas Baudy <niklas.baudy@vanniktech.de>
2017-12-21 16:09:47 +0100

    Update libraries to latest. (#104)

--- src/test/groovy/com/vanniktech/android/junit/jacoco/GenerationPluginSpec.groovy
+++ src/test/groovy/com/vanniktech/android/junit/jacoco/GenerationPluginSpec.groovy
@@ -2,20 +2,31 @@ package com.vanniktech.android.junit.jacoco
 
 import com.android.build.gradle.internal.SdkHandler
 import org.gradle.testfixtures.ProjectBuilder
+import org.junit.Rule
+import org.junit.rules.TemporaryFolder
 import spock.lang.Specification
 import spock.lang.Unroll
 
 final class GenerationPluginSpec extends Specification {
   final static ANDROID_PLUGINS = ["com.android.application", "com.android.library", "com.android.test"]
-  final static COMPILE_SDK_VERSION = 25
-  final static BUILD_TOOLS_VERSION = "25.0.2"
+  final static COMPILE_SDK_VERSION = 27
+  final static BUILD_TOOLS_VERSION = "27.0.1"
   final static APPLICATION_ID = "com.example"
   // Test fixture that emulates a local android sdk
-  final static TEST_ANDROID_SDK = getClass().getResource("/android-sdk/").toURI()
+  final static TEST_ANDROID_SDK = getClass().getResource("/android-sdk").toURI()
   def project
 
+  @Rule public TemporaryFolder tempDir = new TemporaryFolder()
+
   def "setup"() {
-    project = ProjectBuilder.builder().build()
+    File testProjectDir = tempDir.newFolder("testProject")
+    def manifest = new File('src/main/AndroidManifest.xml',testProjectDir)
+    manifest.parentFile.mkdirs()
+    manifest.write("<manifest package=\"com.example.test\">\n<application />\n</manifest>")
+
+    project = ProjectBuilder.builder()
+      .withProjectDir(testProjectDir)
+      .build()
 
     // Set mock test sdk, we only need to test the plugins tasks
     SdkHandler.sTestSdkFolder = project.file TEST_ANDROID_SDK
@@ -98,9 +109,15 @@ final class GenerationPluginSpec extends Specification {
         release {}
       }
 
+      flavorDimensions "number"
+  
       productFlavors {
-        flavor1 {}
-        flavor2 {}
+        flavor1 {
+          dimension "number"
+        }
+        flavor2 {
+          dimension "number"
+        }
       }
     }
 
